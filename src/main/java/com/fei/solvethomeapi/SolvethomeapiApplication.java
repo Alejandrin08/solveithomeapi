04f3cda7eec03606d33633778a69cfad5dfe7e90
Alejandrin08 <zs21013870@estudiantes.uv.mx>
2024-11-06 22:33:32 -0600

    First commit

---
+++ src/main/java/com/fei/solvethomeapi/SolvethomeapiApplication.java
@@ -0,0 +1,13 @@
+package com.fei.solvethomeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolvethomeapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolvethomeapiApplication.class, args);
+    }
+
+}
