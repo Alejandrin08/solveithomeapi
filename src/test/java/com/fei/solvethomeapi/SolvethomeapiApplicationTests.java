09943aed7885f2affa4bced87a72f787ebab5b7c
Alejandrin08 <zs21013870@estudiantes.uv.mx>
2024-11-06 22:33:32 -0600

    First commit

---
+++ src/test/java/com/fei/solvethomeapi/SolvethomeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fei.solvethomeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolvethomeapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
